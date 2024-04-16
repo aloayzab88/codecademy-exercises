41e61fc0f3fea7702dd94ae6fc62c5838ed21e4b
Alejandro Loayza <aloayzab88@gmail.com>
2024-04-16 13:32:25 -0500

    Correciones para longestWordLength y reduce

--- js/arrays-loops/longestWordLength.js
+++ js/arrays-loops/longestWordLength.js
@@ -1,14 +1,20 @@
 const words = ['gato', 'perro', 'elefante', 'jirafa', 'cocodrilo'];
 const otherWords = ['manzana', 'banana', 'naranja', 'uva'];
-
+console.log(otherWords.length);
 const longestWordLength = array => {
     let longestWord = '';
     for (const item of array) {
+        //console.log('item: ' + item, 'length: ' + item.length, longestWord);
         if (item.length > longestWord.length)
             longestWord = item
     }
-    return longestWord.length;
+    return longestWord;
 }
 
 console.log(longestWordLength(words));
 console.log(longestWordLength(otherWords));
+
+const longestWordLengthReduce = array => array.reduce((prev, next) => prev.length < next.length ? next : prev);
+
+console.log(longestWordLengthReduce(words));
+console.log(longestWordLengthReduce(otherWords));
